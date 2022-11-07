aeeaf39e355e4598052aaa112fb031d72a25eced
denis vladimirovoch <117669223+dinyarus@users.noreply.github.com>
2022-11-07 21:11:02 +0300

    Add files via upload

---
+++ mysite.ru/menu.inc.php
@@ -0,0 +1,9 @@
+            <nav>
+                <ul>
+                    <li><a href="#"> Новости </a></li>
+                    <li><a href="#"> Творчество </a></li>
+                    <li><a href="#"> Фото </a></li>
+                    <li><a href="#"> Ссылки </a></li>
+                    <li><a href="#"> Контакты </a></li>
+                </ul>    
+            </nav>
