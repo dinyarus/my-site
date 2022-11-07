9c88ba707fcdd3b7dc1b0c6b7d69391d8f3a43ee
denis vladimirovoch <117669223+dinyarus@users.noreply.github.com>
2022-11-07 20:34:49 +0300

    Delete menu.inc.php

--- menu.inc.php
+++
@@ -1,9 +0,0 @@
-            <nav>
-                <ul>
-                    <li><a href="#"> Новости </a></li>
-                    <li><a href="#"> Творчество </a></li>
-                    <li><a href="#"> Фото </a></li>
-                    <li><a href="#"> Ссылки </a></li>
-                    <li><a href="#"> Контакты </a></li>
-                </ul>    
-            </nav>
